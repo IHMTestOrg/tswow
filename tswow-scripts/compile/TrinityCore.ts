a34f1431a7c2f53c4e34873616fa841fd128d5d1
ihm-tswow <ihm-ts@protonmail.com>
2025-07-31 12:23:52 +0200

    add tsan and ubsan

--- tswow-scripts/compile/TrinityCore.ts
+++ tswow-scripts/compile/TrinityCore.ts
@@ -248,6 +248,8 @@ export namespace TrinityCore {
                 +` -DTRACY_TIMER_FALLBACK="${!Args.hasFlag('tracy-better-timer',[process.argv,args1])?'ON':'OFF'}"`
                 +` -DBUILD_TESTING="OFF"`
                 +` -DASAN="${process.argv.includes('asan')?'ON':'OFF'}"`
+                +` -DTSAN="${process.argv.includes('tsan')?'ON':'OFF'}"`
+                +` -DUBSAN="${process.argv.includes('ubsan')?'ON':'OFF'}"`
                 +` -S "${spaths.cores.TrinityCore.get()}"`
                 +` -B "${bpaths.TrinityCore.get()}"`;
                 buildCommand = `${cmake} --build ${bpaths.TrinityCore.get()} --config ${type}`;
